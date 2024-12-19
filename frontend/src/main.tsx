6ada61221172b913995efd0c47d5e7b6e12a2a37
vvorobjev <v.vorobjev@hq.runpay.com>
2024-12-19 16:10:49 +0300

    added ButtonAppBar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import ButtonAppBar from "./components/ButtonAppBar.tsx";
+import { ThemeProvider } from "@emotion/react";
+import { customTheme } from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <ThemeProvider theme={customTheme}>
+    <ButtonAppBar />
     <App />
-  </StrictMode>
+  </ThemeProvider>
 );
