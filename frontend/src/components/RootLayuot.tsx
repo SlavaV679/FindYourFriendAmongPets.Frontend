05fa9e088388d754333598891fad972ee3a80dd1
vvorobjev <v.vorobjev@hq.runpay.com>
2024-12-24 10:34:12 +0300

    added footer

--- frontend/src/components/RootLayuot.tsx
+++ frontend/src/components/RootLayuot.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import HeaderNavigation from "./HeaderNavigation";
+import Footer from "./Footer";
 
 export function RootLayout() {
   return (
     <div>
       <HeaderNavigation />
       <Outlet />
+      <Footer />
     </div>
   );
 }
